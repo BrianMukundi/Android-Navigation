f33a48261960a69b9c0c7c7b9096201ee73247f5
Brian <brianmukundi67@students.tukenya.ac.ke>
2023-02-08 06:51:00 +0300

    updated

---
+++ app/src/main/java/com/example/navigation_1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.navigation_1
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.ournavhost) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val menu = findViewById<BottomNavigationView>(R.id.bottom_menu)
+        menu.setupWithNavController(navController)
+    }
+}
